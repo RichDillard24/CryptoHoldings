c47bbe4974067ecffc5460c8b970923db4d7b448
RichDillard24 <richiegd@hotmail.com>
2025-12-03 14:50:47 -0800

    Initial Commit

---
+++ CryptoHoldings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoHoldings
+//
+//  Created by Richard Dillard on 12/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
